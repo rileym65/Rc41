ea77c7f533c0250b72ba05434a4903c54ed36347
Mike Riley <mikehriley@hotmail.com>
2024-06-28 16:24:20 +0000

    Mofified how numbers are shown in program lines

--- programline.c
+++ programline.c
@@ -60,6 +60,51 @@ char* Postfix(byte i, int adr, char* buffer) {
   return buffer;
   }
 
+int ProgramNumber(int adr, char* buffer) {
+  int  b;
+  int  p;
+  char mode;
+  char mant[32];
+  char expn[32];
+  char sign;
+  char esign;
+  mode = 'M';
+  p = 0;
+  b = ram[adr];
+  sign = ' ';
+  esign = ' ';
+  while (b >= 0x10 && b <= 0x1c) {
+    if (b <= 0x19) {
+      if (mode == 'M') mant[p++] = '0' + b - 0x10;
+        else expn[p++] = '0'+ b - 0x10;
+      }
+    if (b == 0x1a) mant[p++] = '.';
+    if (b == 0x1b) {
+      if (mode == 'M') {
+        mant[p] = 0;
+        p = 0;
+        mode = 'E';
+        }
+      }
+    if (b == 0x1c) {
+      if (mode == 'M') sign = (sign = ' ') ? '-' : ' ';
+        else esign = (esign = ' ') ? '-' : ' ';
+      }
+    adr--;
+    b = ram[adr];
+    }
+  if (mode == 'M') mant[p] = 0;
+  if (sign == '-') strcat(buffer,"-");
+  strcat(buffer, mant);
+  if (mode == 'E') {
+    expn[p] = 0;
+    strcat(buffer," E");
+    if (esign == '-') strcat(buffer,'-');
+    strcat(buffer,expn);
+    }
+  return adr;
+  }
+
 char* ProgramList(int lineNumber, int adr, char* buffer) {
   int i;
   int b;
@@ -102,14 +147,7 @@ char* ProgramList(int lineNumber, int adr, char* buffer) {
       }
     else {
       if (b >= 0x10 && b <= 0x1c) {
-        strcpy(tmp," ");
-        while (b >= 0x10 && b <= 0x1c) {
-          strcat(tmp, reverse[b].name);
-          adr--;
-          b = ram[adr];
-          }
-        if (tmp[0] == ' ') strcat(buffer, tmp+1);
-          else strcat(buffer, tmp);
+        adr = ProgramNumber(adr, buffer);
         }
       else if (b == 0xae) {
         b2 = ram[adr-1];
@@ -206,115 +244,7 @@ char* ProgramLine(char* buffer) {
     }
   else {
     adr--;
-    while (ram[adr] == 0) adr--;
-    if (lineNumber < 100) sprintf(buffer, "%02d ", lineNumber);
-      else sprintf(buffer,"%d ", lineNumber);
-    
-    b = ram[adr];
-    if (b >= 0xc0 && b < 0xce) {
-      if (ram[adr-2] < 0xf0) {
-        if ((ram[adr-2] & 0xf0) == 0x20) {
-          sprintf(buffer,".END. REG %d", end - 0x0c0);
-          }
-        else{
-          if (lineNumber < 100) sprintf(buffer, "%02d END", lineNumber);
-            else sprintf(buffer,"%d END", lineNumber);
-          }
-        }
-      else {
-        if (lineNumber < 100) sprintf(buffer, "%02d ", lineNumber);
-          else sprintf(buffer,"%d ", lineNumber);
-        strcat(buffer,"LBL\"");
-        adr -= 2;
-        b = ram[adr] - 1;
-        b &= 0xf;
-        adr -= 2;
-        for (i=0; i<b; i++) {
-          if (ram[adr] == 0) tmp[0] = '_';
-            else tmp[0] = ram[adr];
-          adr--;
-          tmp[1] = 0;
-          strcat(buffer,tmp);
-          }
-        }
-      }
-    else {
-      if (b >= 0x10 && b <= 0x1c) {
-        strcpy(tmp," ");
-        while (b >= 0x10 && b <= 0x1c) {
-          strcat(tmp, reverse[b].name);
-          adr--;
-          b = ram[adr];
-          }
-        if (tmp[0] == ' ') strcat(buffer, tmp+1);
-          else strcat(buffer, tmp);
-        }
-      else if (b == 0xae) {
-        b2 = ram[adr-1];
-        if (b2 >= 0x80) {
-          sprintf(buffer,"%02d XEQ IND %02d",lineNumber, b2 & 0x7f);
-          }
-        else {
-          sprintf(buffer,"%02d GTO IND %02d",lineNumber, b2 & 0x7f);
-          }
-        }
-      else if (b >= 0xa0 && b <= 0xa7) {
-        b2 = ram[adr-1];
-        i = 0;
-        while (reverse2[i].cmd != 0xff && (reverse2[i].cmd != b || reverse2[i].post != b2)) i++;
-        if (reverse2[i].cmd != 0xff) {
-          sprintf(buffer,"%02d %s",lineNumber, reverse2[i].name);
-          }
-        else {
-          b = ((b & 0x0f) << 2) | ((b2 & 0xc0) >> 6);
-          b2 &= 0x3f;
-          sprintf(buffer,"%02d XROM %02d,%02d",lineNumber,b,b2);
-          }
-        }
-      else if (b < 0xf0 && (reverse[b].size & 0x0f) == 1) {
-        sprintf(tmp, "%s", reverse[b].name);
-        strcat(buffer, tmp);
-        }
-      else if (b < 0xf0 && (reverse[b].size & 0x0f) == 2) {
-        Postfix(b, adr, buffer);
-        }
-      else if ((reverse[b].size & 0xf0) == 0x60) {
-        if (ram[adr-2] >= 102 && ram[adr-2] <= 111)
-          sprintf(tmp, "%s %c", reverse[b].name, ram[adr-2] - 102 + 'A');
-        else if (ram[adr-2] >= 123 && ram[adr-2] <= 127)
-          sprintf(tmp, "%s %c", reverse[b].name, ram[adr-2] - 123 + 'a');
-        else
-          sprintf(tmp, "%s %02d", reverse[b].name, ram[adr-2] & 0x7f);
-        strcat(buffer, tmp);
-        }
-      else if ((reverse[b].size & 0xf0) == 0x10) {
-        sprintf(tmp, "%s", reverse[b].name);
-        strcat(buffer, tmp);
-        b = ram[--adr] & 0x0f;
-        strcat(buffer,"\"");
-        for (i=0; i<b; i++) {
-          tmp[0] = ram[--adr];
-          tmp[1] = 0;
-          strcat(buffer,tmp);
-          }
-        }
-      else if (b >= 0xf0) {
-        adr--;
-        buffer[strlen(buffer)-1] = '"';
-        tmp[1] = 0;
-        for (i=0; i<(b&0x0f); i++) {
-          if (ram[adr] == 0x7f) { tmp[0] = '|'; adr--; }
-          else if (ram[adr] == 0x00) { tmp[0] = '_'; adr--; }
-          else if (ram[adr] < ' ' || ram[adr] > 0x7e) {
-            tmp[0] = '*'; adr--;
-            }
-            else tmp[0] = ram[adr--];
-          strcat(buffer, tmp);
-          }
-        }
-      else
-sprintf(buffer, "b=%02x, size=%d",b,reverse[b].size);
-      }
+    ProgramList(lineNumber, adr, buffer);
     }
 
   return buffer;
