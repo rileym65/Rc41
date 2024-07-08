210019614e182500076f240930b7b4f8c4418258
Mike Riley <mikehriley@hotmail.com>
2024-07-08 13:37:31 +0000

    Adjust key assignments when loading program from card

--- rprg.c
+++ rprg.c
@@ -3,6 +3,7 @@
 void Rprg(char *filename) {
   int file;
   int address;
+  int sadr;
   int nabc;
   int len;
   int regs;
@@ -17,6 +18,7 @@ void Rprg(char *filename) {
     address = ((ram[REG_C+1] & 0x0f) << 8) | ram[REG_C+0];
     address *= 7;
     address += 2;
+    sadr = address;
     while (len > 0) {
       nabc = ToPtr(address);
       ram[REG_B+1] = (nabc >> 8) & 0xff;
@@ -28,7 +30,22 @@ void Rprg(char *filename) {
       }
     close(file);
     ReLink();
+
+    while (ram[sadr] == 0x00) sadr--;
+    while (ram[sadr] < 0xc0 || ram[sadr] > 0xcd || ram[sadr-2] >= 0xf0) {
+      if (ram[sadr] >= 0xc0 && ram[sadr] <= 0xcd && ram[sadr-2] >= 0xf0) {
+        if (ram[sadr-3] != 0x00) {
+          b = ram[sadr-3];
+          UnAsn(b, 3);
+          ram[sadr-3] = b;
+          SetKaFlag(b, 1);
+          }
+        }
+      sadr -= isize(sadr);
+      }
+
     Pack();
+
     }
   else {
     Message("CARD ERR");
