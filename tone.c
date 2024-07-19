f61f2f74d444dbef33d78898c11dff315e6d766a
Mike Riley <mikehriley@hotmail.com>
2024-07-19 20:00:44 +0000

    Check for alpha data on TONE

--- tone.c
+++ tone.c
@@ -4,6 +4,11 @@ void Tone(byte n) {
   NUMBER a;
   if (n >= 0x80 && n <= 0xef) {
     a = Rcl(n & 0x7f);
+    if (a.sign != 9 && a.sign != 0) {
+      Message("ALPHA DATA");
+      Error();
+      return;
+      }
     n = ToInteger(a);
     if (n >= 0x0a) {
       Message("DATA ERRROR");
